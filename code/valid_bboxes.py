23bbc81834c4378ff2bf024fc1bbea421b0728fc
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-11-21 20:54:04 +0000

    added undo z

--- code/valid_bboxes.py
+++ code/valid_bboxes.py
@@ -18,50 +18,83 @@ cnt = 0
 
 # undo 'z'
 
+bins = []
+
 for season in seasons:
     data = [annot for annot in json_data if annot['annotation']['season'] == season ]
     locations = list(set([annot['annotation']['location'] for annot in data]))
 
     for location in locations:
-        cnt += 1
-        print(cnt, season, location)
+        bins.append({'season': season, 'location': location})
 
-        location_bboxes = [annot for annot in data if annot['annotation']['location'] == location]
-        #image = cv2.imread('../data/serengeti_bboxes/images/'+image_id + '.JPG')
-        images = list(set([bbox['image_id'] for bbox in location_bboxes]))
 
-        validated = False
+seen = []
 
-        while not validated:
+while bins:
+    bin = bins[0]
+    bins = bins[1:]
+    season = bin['season']
+    location = bin['location']
+    seen.append({'season': season, 'location': location})
 
-            for image_id in images:
-                image = cv2.imread('../data/serengeti_bboxes/images/'+image_id.split('/')[-1] + '.JPG')
-                bboxes = [bbox for bbox in location_bboxes if bbox['image_id'] == image_id]
 
-                for bbox in bboxes:
-                    x, y, w, h = bbox['bbox']
-                    #image = cv2.imread('../data/serengeti_bboxes/images/'+bbox['image_id'].split('/')[-1] + '.JPG')
-                    cv2.rectangle(image, (int(x), int(y)), (int(x+w), int(y+h)), (255, 255, 0), 3)
+    print(len(seen), len(bins), season, location)
+
+    location_bboxes = [annot for annot in json_data if annot['annotation']['location'] == location and annot['annotation']['season'] == season]
+    #image = cv2.imread('../data/serengeti_bboxes/images/'+image_id + '.JPG')
+    images = list(set([bbox['image_id'] for bbox in location_bboxes]))
+
+    validated = False
+
+    while not validated:
+
+        for image_id in images:
+            image = cv2.imread('../data/serengeti_bboxes/images/'+image_id.split('/')[-1] + '.JPG')
+            bboxes = [bbox for bbox in location_bboxes if bbox['image_id'] == image_id]
+
+            for bbox in bboxes:
+                x, y, w, h = bbox['bbox']
+                #image = cv2.imread('../data/serengeti_bboxes/images/'+bbox['image_id'].split('/')[-1] + '.JPG')
+                cv2.rectangle(image, (int(x), int(y)), (int(x+w), int(y+h)), (255, 255, 0), 3)
+            try:
+                cv2.imshow('image', image)
+            except:
+                continue
+                print('file error')
+
+            key = cv2.waitKey(0)
+            print(key)
+            if key == 32:
+                continue
+            elif key == 107:
+                # keep
+                keep.append({'season': season, 'location': location})
+                validated = True
+                break
+            elif key == 100:
+                # discard
+                discard.append({'season': season, 'location': location})
+                validated = True
+                break
+            elif key == 122:
+                # undo
+                last = seen.pop()
+                last = seen.pop()
                 try:
-                    cv2.imshow('image', image)
+                    discard.remove(last)
                 except:
-                    continue
-                    print('file error')
+                    pass
+                try:
+                    keep.remove(last)
+                except:
+                    pass
+                bins = [last] + bins
+                validated = True
+                break
 
-                key = cv2.waitKey(0)
-                if key == 32:
-                    continue
-                elif key == 107:
-                    # keep
-                    keep.append({'season': season, 'location': location})
-                    validated = True
-                    break
-                elif key == 100:
-                    # discard
-                    discard.append({'season': season, 'location': location})
-                    validated = True
-                    break
+    print('keep', keep)
+    print('discard', discard)
 
-        json.dump(discard, open('discard.json', 'w'))
-        json.dump(keep, open('keep.json', 'w'))
-print(cnt)
+    json.dump(discard, open('discard.json', 'w'))
+    json.dump(keep, open('keep.json', 'w'))
+
