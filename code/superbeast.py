a6f2691f1f255813dccb65e7633926a3ff15cde0
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-09 13:45:46 +0000

    superbeast 102

--- code/superbeast.py
+++ code/superbeast.py
@@ -5,13 +5,13 @@ def run(command):
     #os.system(command)
 
 
-def main(experiment_dir, species):
+def main(experiment_dir, species, n_augment):
 
     k = 4
 
     print("\nGenerating %i FOLD experiment in %s for the following species :\n\t%s\n"%(k, experiment_dir, ', '.join(species)))
 
-    j = 1000
+    j = n_augment
 
     try:
         os.mkdir(experiment_dir)
@@ -30,31 +30,34 @@ def main(experiment_dir, species):
         run("python3 meta_experiment_reloaded.py -e %s --n_images=%i --species=%s"%(fold_dir, i, ','.join(species)))
         run("python3 download_empty_reloaded.py -e %s"%fold_dir)
         run("python3 augment_trainset_reloaded.py -e %s -i %i"%(fold_dir, j))
-        run("cd %s; sbatch sbatch_train.sh; sbatch sbatch_augment_%i.sh; cd -;"%(fold_dir, i))
+        run("cd %s; sbatch sbatch_train.sh; sbatch sbatch_augment_%i.sh; cd -;"%(fold_dir, j))
 
 
 if __name__ == "__main__":
     import sys, getopt
 
     experiment_dir = ''
-    n_images = 0
+    n_augment = 0
     argv = sys.argv[1:]
 
     try:
-        opts, args = getopt.getopt(argv, "he:s:", ["experiment_dir=", "species="])
+        opts, args = getopt.getopt(argv, "he:s:n:", ["experiment_dir=", "species=", "n_augment="])
     except getopt.GetoptError:
-        print('script.py -e <experiment_dir> -s <species>')
+        print('script.py -e <experiment_dir> -s <species> -n <n_augment>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('script.py -e <experiment_dir> -s <species>')
+            print('script.py -e <experiment_dir> -s <species> -n <n_augment>')
             sys.exit()
         elif opt in ("-e", "--experiment_dir"):
             experiment_dir = arg
         elif opt in ("-s", "--species"):
             species = [s.strip().lower() for s in arg.split(',')]
+        elif opt in ("-n", "--n_augment"):
+            n_augment = int(arg)
 
     if not experiment_dir[-1] == '/':
         experiment_dir += '/'
-    main(experiment_dir, species)
+
+    main(experiment_dir, species, n_augment)
 
