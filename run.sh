76fa12e721b70b99b3c92539bf5cfc7d508ee822
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-13 08:08:41 +0000

    all species

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source darwin_venv/bin/activate/
 cd src/
-python3 superbeast.py -e ../data/experiments/test -s lionmale,gazellethomsons -n 100
+python3 superbeast.py -e ../data/experiments/all -n 100 -s aardvark,aardwolf,baboon,batearedfox,buffalo,bushbuck,caracal,cheetah,civet,dikdik,eland,elephant,empty,gazellegrants,gazellethomsons,genet,giraffe,guineafowl,hare,hartebeest,hippopotamus,honeybadger,human,hyenaspotted,hyenastriped,impala,jackal,koribustard,leopard,lionfemale,lionmale,mongoose,monkeyvervet,ostrich,otherbird,porcupine,reedbuck,reptiles,rhinoceros,rodents,secretarybird,serval,topi,warthog,waterbuck,wildcat,wildebeest,zebra,zorilla
