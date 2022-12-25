25f23f9190c118f71d885e2111205853fd519dd3
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-25 18:45:04 +0100

    one test

--- src/validate.py
+++ src/validate.py
@@ -9,7 +9,7 @@ def main(dir):
     #     for test in ["raw_1"] + ["augment_1_" + str(i) for i in [1,2,4,8]]:
     fold = "fold_14"
     test = "raw_1"
-    os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> %sresults/%s_%s.out;cd .."%(dir+fold, test, test, dir, fold, test))
+    os.system("cd %s; python3 ../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd -"%(dir+fold, test, test, dir, fold, test))
 
 if __name__=="__main__":
     main("../data/experiments/montecarlo/")
