83f96c79b9aa5f8267cb92c25fa60cfdff0d04e6
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-25 18:42:51 +0100

    one test

--- src/validate.py
+++ src/validate.py
@@ -1,6 +1,15 @@
 import os
 
-for fold in ["fold_" + str(i) for i in range(11,21)]:
-    for test in ["raw_1"] + ["augment_1_" + str(i) for i in [1,2,4,8]]:
-        os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd .."%(fold, test, test, fold, test))
+def main(dir):
+    # for fold in [dir + "fold_" + str(i) for i in range(11,21)]:
+    #     for test in ["raw_1"] + ["augment_1_" + str(i) for i in [1,2,4,8]]:
+    #         os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd .."%(fold, test, test, fold, test))
 
+    # for fold in [dir + "fold_" + str(i) for i in range(11,21)]:
+    #     for test in ["raw_1"] + ["augment_1_" + str(i) for i in [1,2,4,8]]:
+    fold = "fold_14"
+    test = "raw_1"
+    os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> %sresults/%s_%s.out;cd .."%(dir+fold, test, test, dir, fold, test))
+
+if __name__=="__main__":
+    main("../data/experiments/montecarlo/")
