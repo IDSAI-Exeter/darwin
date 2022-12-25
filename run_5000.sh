cd61e02895ac9a09414c0f4b9d81bd3dd0027a99
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-25 23:19:27 +0100

    renamed superbeast and changed k to 1, will set it as argument

--- run_5000.sh
+++ run_5000.sh
@@ -1,3 +1,3 @@
 source darwin_venv/bin/activate
 cd src/
-python3 superbeast.py -e ../data/experiments/montecarlo_5000 -r 100,300,500 -a 1,2,4 -n 100 -s baboon,eland,elephant,lionfemale,topi,wildebeest
+python3 montecarlo.py -e ../data/experiments/montecarlo_5000 -r 100,300,500 -a 1,2,4 -n 100 -s baboon,eland,elephant,lionfemale,topi,wildebeest
