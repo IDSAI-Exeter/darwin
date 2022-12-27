5f419a9a9933f0aa3afd8b507995c16b97f5caf4
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-27 10:11:06 +0100

    if result file does not exist then validate

--- src/validate.py
+++ src/validate.py
@@ -1,11 +1,19 @@
 import os
+import os.path
+
 
 def main(dir):
+    try:
+        os.mkdir("%sresults"%dir)
+    except:
+        pass
     for fold in ["fold_" + str(i) for i in range(11,21)]:
         for test in ["raw_1"]:
-            os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/raw/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd -" % (dir + fold, test, test, fold, test))
+            if not os.path.exists("%sresults/%s_%s.out"%(dir, fold, test)):
+                os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/raw/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd -" % (dir + fold, test, test, fold, test))
         for test in ["augment_1_" + str(i) for i in [1,2,4,8]]:
-            os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd .."%(dir + fold, test, test, fold, test))
+            if not os.path.exists("%sresults/%s_%s.out"%(dir, fold, test)):
+                os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd .."%(dir + fold, test, test, fold, test))
 
     # for fold in [dir + "fold_" + str(i) for i in range(11,21)]:
     #     for test in ["raw_1"] + ["augment_1_" + str(i) for i in [1,2,4,8]]:
@@ -13,6 +21,7 @@ def main(dir):
     #     # fold = "fold_14"
     #     test = "raw_1"
     #     os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/raw/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd -"%(dir+fold, test, test, fold, test))
+
 
 if __name__=="__main__":
     main("../data/experiments/montecarlo/")
