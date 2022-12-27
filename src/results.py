a652fe7057ef664b56db486e3c104ea62fc2e760
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-27 15:55:02 +0100

    species

--- src/results.py
+++ src/results.py
@@ -37,12 +37,12 @@ def main(dir, raw_sizes, aug_factors):
         for r in ["raw_" + str(i) for i in raw_sizes]:
             raw = parse(dir + "%s_%s.out"%(fold, r))
             if raw is not None:
-                species_list = list(raw['Class'])[0:]
                 for test in ["augment_1_" + str(i) for i in aug_factors]:
                     augment = parse(dir + "%s_%s.out"%(fold, test))
                     if augment is not None:
                         delta.append(float(augment.iloc[0]['mAP50-95']) - float(raw.iloc[0]['mAP50-95']))
                         if test == "augment_1_1":
+                            species_list = list(augment['Class'])[1:]
                             species = []
                             print(augment)
                             for i in range(1, len(augment)):
