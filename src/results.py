ffa2271ec2b6cb7538f43b5cf5c790a653ab3d34
cedricidsai <c.s.mesnage@exeter.ac.uk>
2022-12-29 21:20:17 +0100

    32

--- src/results.py
+++ src/results.py
@@ -57,6 +57,7 @@ def main(dir, raw_sizes, aug_factors):
 
     species_list = list(dfs[0]['Class'])[1:]
     df_species = pd.DataFrame(deltas_species)
+    print(df_species)
     df_species.columns = species_list
     print(df_species)
     print(df_species.mean(axis=0))
