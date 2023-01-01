c3951d19b27bdcfd31afa6accba7232202ad4ba7
cedricidsai <c.s.mesnage@exeter.ac.uk>
2023-01-01 13:13:19 +0100

    superbeast 101, in flight commit

--- src/plot.py
+++ src/plot.py
@@ -4,20 +4,22 @@ import os
 import pandas
 
 
-def main(experiment_dir, n_augment, timings):
-
-    k = 10
+def main(experiment_dir, k, aug_factors, timings, raw_size):
 
     l = []
 
-    for i in range(1, k+1):
-        print(experiment_dir + "fold_%i"%(10+i))
-        l += [('t', "%sfold_%i/raw_1/runs/raw/results.csv"%(experiment_dir, 10+i), 10+i)]
-        l += [('a', "%sfold_%i/augment_1_%i/runs/augment/results.csv"%(experiment_dir, 10+i, n_augment), 10+i)]
+    download = False
 
-    for t, f, i in l:
-        os.system("scp -i ~/.ssh/id_rsa_jade ccm30-dxa01@jade2.hartree.stfc.ac.uk:/jmain02/home/J2AD013/dxa01/ccm30-dxa01/%s csv/fold_%s_%i.csv"%(f, t, i))
-        print("scp -i ~/.ssh/id_rsa_jade ccm30-dxa01@jade2.hartree.stfc.ac.uk:/jmain02/home/J2AD013/dxa01/ccm30-dxa01/%s csv/fold_%s_%i.csv"%(f, t, i))
+    if download:
+        for i in range(1, k+1):
+            print(experiment_dir + "fold_%i"%(10+i))
+            l += [('t', "%sfold_%i/raw_%i/runs/raw/results.csv"%(experiment_dir, 10+i, raw_size), 10+i)]
+            for j in aug_factors:
+                l += [('a_%i'%j, "%sfold_%i/augment_1_%i/runs/augment/results.csv"%(experiment_dir, 10+i, j), 10+i)]
+
+        for t, f, i in l:
+            os.system("scp -i ~/.ssh/id_rsa_jade ccm30-dxa01@jade2.hartree.stfc.ac.uk:/jmain02/home/J2AD013/dxa01/ccm30-dxa01/%s csv/fold_%s_%i.csv"%(f, t, i))
+            print("scp -i ~/.ssh/id_rsa_jade ccm30-dxa01@jade2.hartree.stfc.ac.uk:/jmain02/home/J2AD013/dxa01/ccm30-dxa01/%s csv/fold_%s_%i.csv"%(f, t, i))
 
     import pandas as pd
     import matplotlib.pyplot as plt
@@ -26,11 +28,12 @@ def main(experiment_dir, n_augment, timings):
     i_ = [10 + i for i in range(1, k+1)]
 
     for i in i_:
-        try:
-            dfs.append(('raw_', pd.read_csv("csv/fold_t_%i.csv"%i), i))
-            dfs.append(('aug_', pd.read_csv("csv/fold_a_%i.csv"%i), i))
-        except:
-            pass
+        # try:
+        dfs.append(('raw_%i'%raw_size, pd.read_csv("csv/fold_t_%i.csv"%i).iloc[0:300], i))
+        for j in aug_factors:
+            dfs.append(('aug_%i'%j, pd.read_csv("csv/fold_a_%i_%i.csv"%(j, i)).iloc[0:300], i))
+        # except:
+        #     pass
     print(dfs)
     def cs(secs, df):
         ts = [secs]*len(df)
@@ -41,9 +44,12 @@ def main(experiment_dir, n_augment, timings):
 
     i = 0
     while i < len(dfs):
+        print(i, len(dfs))
         dfs[i][1]['cumtime'] = cs(timings[0], dfs[i][1])  # 100 raw
-        dfs[i+1][1]['cumtime'] = cs(timings[1], dfs[i+1][1])  # 100 aug
-        i += 2
+        for j in range(1, len(aug_factors) + 1):
+            print(i+j, len(dfs))
+            dfs[i+j][1]['cumtime'] = cs(timings[j], dfs[i+j][1])  # 100 aug
+        i += len(aug_factors) + 1
 
     # Proper values to be read from log files.
 
@@ -69,7 +75,7 @@ def main(experiment_dir, n_augment, timings):
     fig, ax = plt.subplots()
     for t, df, i in dfs:#[:4]:
         df[t+str(i)] = df['metrics/mAP_0.5:0.95']
-        df.plot(ax=ax, x='cumtime', y=t+str(i), legend=True, title='Superbeast 101 augmented with %i images per species'%n_augment)
+        df.plot(ax=ax, x='cumtime', y=t+str(i), legend=True, title='Superbeast 101')
         print(df.columns)
     plt.ylabel('metrics/mAP_0.5:0.95')
     plt.xlabel('time(s)')
@@ -78,41 +84,52 @@ def main(experiment_dir, n_augment, timings):
     fig, ax = plt.subplots()
     for t, df, i in dfs:#[:4]:
         df[t+str(i)] = df['metrics/mAP_0.5:0.95']
-        df[t+str(i)].plot(legend=True, title='Superbeast 101 augmented with %i images per species'%n_augment)
+        df[t+str(i)].plot(legend=True, title='Superbeast 101')
     plt.xlabel('epochs')
     plt.ylabel('metrics/mAP_0.5:0.95')
     fig.savefig('epochs.png')
 
     kfold = {}
-    kfold['raw_'] = pandas.DataFrame()
-    kfold['aug_'] = pandas.DataFrame()
+    kfold['raw_%i'%raw_size] = pandas.DataFrame()
     fig, ax = plt.subplots()
-    for t, df, i in dfs:#[:4]:
+
+    for i in aug_factors:
+        kfold['aug_%i'%i] = pandas.DataFrame()
+
+    for t, df, i in dfs:
         df[t+str(i)] = df['metrics/mAP_0.5:0.95']
         # df[t+str(i)].plot(legend=True, title='Superbeast 101 augmented with %i images per species'%n_augment)
+        print(t)
         kfold[t][i] = df['metrics/mAP_0.5:0.95']
 
-    kfold['raw_']['raw'] = kfold['raw_'].mean(axis=1)
-    kfold['aug_']['raw+aug'] = kfold['aug_'].mean(axis=1)
-    kfold['raw_']['std'] = kfold['raw_'].std(axis=1)
-    kfold['aug_']['std'] = kfold['aug_'].std(axis=1)
-
-    kfold['raw_']['cumtime'] = cs(timings[0], kfold['raw_'])
-    kfold['aug_']['cumtime'] = cs(timings[1], kfold['aug_'])
-
-    # kfold['raw_'].plot(ax=ax, use_index=True, y='raw', color='black', title='K-fold cross validation')
-    # kfold['aug_'].plot(ax=ax, use_index=True, y='raw+aug', color='gray')
-    kfold['raw_'].plot(ax=ax, x='cumtime', y='raw', linestyle='solid', color='black') #, title='MonteCarlo Shuffle split cross validation augmented with %i images per species'%n_augment)
-    kfold['aug_'].plot(ax=ax, x='cumtime', y='raw+aug', linestyle='dashdot', color='black')
+    kfold['raw_%i'%raw_size]['raw'] = kfold['raw_%i'%raw_size].mean(axis=1)
+    kfold['raw_%i'%raw_size]['std'] = kfold['raw_%i'%raw_size].std(axis=1)
+    kfold['raw_%i'%raw_size]['cumtime'] = cs(timings[0], kfold['raw_%i'%raw_size])
 
     import math
+    # kfold['raw_%i'%raw_size].plot(ax=ax, use_index=True, y='raw', color='black', title='K-fold cross validation')
+    # kfold['aug_'].plot(ax=ax, use_index=True, y='raw+aug', color='gray')
+
+    kfold['raw_%i'%raw_size].plot(ax=ax, x='cumtime', y='raw', linestyle='solid', color='black', title='%i-fold Monte Carlo cross validation mean mAP per epoch'%k)
+    plt.fill_between(color='lightgray', x=kfold['raw_%i'%raw_size]['cumtime'], y1=kfold['raw_%i'%raw_size]['raw'] - kfold['raw_%i'%raw_size]['std']/(2*math.sqrt(k)), y2=kfold['raw_%i'%raw_size]['raw'] + kfold['raw_%i'%raw_size]['std']/(2*math.sqrt(k)))
+
+    linestyles = ['dashed', 'dashdot', 'dotted']
+    # styles = {1 : 0, 2 : 1, 4 : 2, 8 : 3}
+    styles = {i: j for i, j in zip(aug_factors, range(len(aug_factors)))}
+
+    for j in aug_factors:
+        kfold['aug_%i'%j]['std'] = kfold['aug_%i'%j].std(axis=1)
+        kfold['aug_%i'%j]['raw_%i+aug_%i'%(raw_size,j)] = kfold['aug_%i'%j].mean(axis=1)
+        kfold['aug_%i'%j]['cumtime'] = cs(timings[1], kfold['aug_%i'%j])
+        print(kfold['aug_%i'%j])
+        kfold['aug_%i'%j].plot(ax=ax, x='cumtime', y='raw_%i+aug_%i'%(raw_size, j), linestyle=linestyles[styles[j]], color='black')
+        plt.fill_between(color='lightgray', x=kfold['aug_%i'%j]['cumtime'], y1=kfold['aug_%i'%j]['raw_%i+aug_%i'%(raw_size, j)] - kfold['aug_%i'%j]['std']/(2*math.sqrt(k)), y2=kfold['aug_%i'%j]['raw_%i+aug_%i'%(raw_size, j)] + kfold['aug_%i'%j]['std']/(2*math.sqrt(k)))
+
 
     # plt.fill_between(x=kfold['aug_'].index, y1=kfold['aug_']['raw+aug'] - kfold['aug_']['std'], y2=kfold['aug_']['raw+aug'] + kfold['aug_']['std'])
-    # plt.fill_between(x=kfold['raw_'].index, y1=kfold['raw_']['raw'] - kfold['raw_']['std'], y2=kfold['raw_']['raw'] + kfold['raw_']['std'])
-    plt.fill_between(color='lightgray', x=kfold['aug_']['cumtime'], y1=kfold['aug_']['raw+aug'] - kfold['aug_']['std']/(2*math.sqrt(k)), y2=kfold['aug_']['raw+aug'] + kfold['aug_']['std']/(2*math.sqrt(k)))
-    plt.fill_between(color='lightgray', x=kfold['raw_']['cumtime'], y1=kfold['raw_']['raw'] - kfold['raw_']['std']/(2*math.sqrt(k)), y2=kfold['raw_']['raw'] + kfold['raw_']['std']/(2*math.sqrt(k)))
+    # plt.fill_between(x=kfold['raw_%i'%raw_size].index, y1=kfold['raw_%i'%raw_size]['raw'] - kfold['raw_%i'%raw_size]['std'], y2=kfold['raw_%i'%raw_size]['raw'] + kfold['raw_%i'%raw_size]['std'])
 
-    yerr = kfold['raw_']['std']
+    # yerr = kfold['raw_%i'%raw_size]['std']
 
     # plt.xlabel('epochs')
     plt.xlabel('time(s)')
@@ -149,5 +166,10 @@ if __name__ == "__main__":
     timings = [13*60+8, 33*60+7, 13*60+8, 33*60+7, 13*60+8, 33*60+7, 13*60+8, 33*60+7]
     timings = [35, 41]
     # main('projects/darwin/data/experiments/montecarlo_/', 1, timings)
-    main(experiment_dir, 1, timings)
+    # main(experiment_dir, 1, timings)
+    timings = [35, 41, 60, 90]
+    raw_size = 1
+    aug_factors = [1, 2, 4]
+    k = 5
+    main(experiment_dir, k, aug_factors, timings, raw_size)
     os.system("mv montecarlo-shuffle.png epochs.png time.png ../plots/")
