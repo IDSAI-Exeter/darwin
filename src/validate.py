16d70118d26ed7ad4bdd068082f0c18e451e7350
cedricidsai <c.s.mesnage@exeter.ac.uk>
2023-01-02 15:46:57 +0000

    add raw sizes validation

--- src/validate.py
+++ src/validate.py
@@ -2,16 +2,16 @@ import os
 import os.path
 
 
-def main(dir, aug_factors):
+def main(dir, aug_factors, raw_sizes):
     try:
         os.mkdir("%sresults"%dir)
     except:
         pass
     for fold in ["fold_" + str(i) for i in range(11,21)]:
-        for test in ["raw_1"]:
+        for test in ["raw_" + str(i) for i in raw_sizes]:
             if not os.path.exists("%sresults/%s_%s.out"%(dir, fold, test)):
                 os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/raw/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd -" % (dir + fold, test, test, fold, test))
-        for test in ["augment_1_" + str(i) for i in aug_factors]:
+        for test in ["augment_" + str(i) + "_" + str(j) for i in raw_sizes for j in aug_factors]:
             if not os.path.exists("%sresults/%s_%s.out"%(dir, fold, test)):
                 os.system("cd %s; python3 ../../../../lib/yolov5/val.py --data %s.yaml --weights %s/runs/augment/weights/best.pt --project validate/ --verbose --task test 2> ../results/%s_%s.out;cd .."%(dir + fold, test, test, fold, test))
 
@@ -28,10 +28,11 @@ if __name__ == "__main__":
 
     experiment_dir = ''
     aug_factors = [1,2,4]
+    raw_sizes = [1,2]
     argv = sys.argv[1:]
 
     try:
-        opts, args = getopt.getopt(argv, "he:a:", ["experiment_dir="])
+        opts, args = getopt.getopt(argv, "he:a:r:", ["experiment_dir="])
     except getopt.GetoptError:
         print('script.py -e <experiment_dir> -a <aug_factors>')
         sys.exit(2)
@@ -43,9 +44,11 @@ if __name__ == "__main__":
             experiment_dir = arg
         elif opt in ("-a", "--aug_factors"):
             aug_factors = [int(s.strip().lower()) for s in arg.split(',')]
+        elif opt in ("-r", "--raw_sizes"):
+            raw_sizes = [int(s.strip().lower()) for s in arg.split(',')]
 
     if not experiment_dir[-1] == '/':
         experiment_dir += '/'
 
     # main("../data/experiments/montecarlo/")
-    main(experiment_dir, aug_factors)
+    main(experiment_dir, aug_factors, raw_sizes)
